67c9d11cd27bf79596049f31424304773e0734af
rosalyn.rowe <rosalyn.rowe@khoros.com>
2021-02-18 13:34:33 -0800

    updates from sandbox

--- src/AttchmentEditor.tsx
+++ src/AttchmentEditor.tsx
@@ -1,6 +1,7 @@
 import { DropzoneOptions, useDropzone } from "react-dropzone";
+// import { DropzoneOptions, useDropzone } from "./useDropzone/useDropzone";
 import classnames from "classnames";
-import useDropzoneWrapper from "./useDropzoneWrapper";
+// import useDropzoneWrapper from "./useDropzoneWrapper";
 
 export default function AttachmentEditor() {
   const options: DropzoneOptions = {
@@ -14,14 +15,14 @@ export default function AttachmentEditor() {
     isDragReject
   } = useDropzone(options);
 
-  const { isDragging, isDragWouldAccept } = useDropzoneWrapper(options);
+  // const { isDragging, isDragWouldAccept } = useDropzoneWrapper(options);
 
   const classes = classnames("lia-attachment-editor", {
-    "lia-drag-accept": isDragging && isDragAccept,
-    "lia-drag-active": isDragging && isDragActive,
-    "lia-drag-reject": isDragging && isDragReject,
-    "lia-dragging": isDragging,
-    "lia-drag-would-reject": isDragging && !isDragWouldAccept
+    "lia-drag-accept": isDragAccept,
+    "lia-drag-active": isDragActive,
+    "lia-drag-reject": isDragReject
+    // "lia-dragging": isDragging,
+    // "lia-drag-would-reject": !isDragWouldAccept
   });
 
   function DragOverlay() {
@@ -32,13 +33,20 @@ export default function AttachmentEditor() {
     );
   }
 
-  console.log(classes, isDragging, isDragReject);
-
   return (
-    <div className={classes} {...getRootProps()}>
-      {isDragging && <DragOverlay />}
-      <input {...getInputProps()} />
-      Add link...
-    </div>
+    <>
+      <div className={classes} {...getRootProps()}>
+        <DragOverlay />
+        <input {...getInputProps()} />
+        Add link...
+      </div>
+      <ul>
+        {/* <li>isDragging: {isDragging.toString()}</li> */}
+        <li>isDragAccept: {isDragAccept.toString()}</li>
+        <li>isDragActive: {isDragActive.toString()}</li>
+        <li>isDragReject: {isDragReject.toString()}</li>
+        {/* <li>isDragWouldAccept: {isDragWouldAccept.toString()}</li> */}
+      </ul>
+    </>
   );
 }
